da7f90485f70a094bd4d5692f6327621b4f26a8a
Pierre Axel Izerable <pierreaxel.iz@gmail.com>
2024-07-01 09:59:06 +0200

    MAJ

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,9 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
   end
 end
